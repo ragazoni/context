3595aa940cd4864c472b5904210e0d409a1fb18e
Adriana Diniz <dri.ragazoni@gmail.com>
2024-02-05 22:31:02 -0300

    create server

---
+++ context/server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080", nil)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
+}
